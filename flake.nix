66b6aceca72da6c7fc47c0680bd25e543711fc0b
Zahary Karadjov <zahary@gmail.com>
2024-05-28 19:09:10 +0300

    feat(nix): Add support for building on ARM and macOS

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux"];
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       perSystem = {pkgs, ...}: {
         devShells.default = import ./shell.nix {inherit pkgs;};
       };
