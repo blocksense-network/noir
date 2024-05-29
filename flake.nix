e9307de8edf46052ccc90c73945a54fcd84c5a4e
Stan Manilov <stanislav.manilov@gmail.com>
2024-05-29 18:27:57 +0300

    fixes(nix): Drop rustup and select Rust the Nix way
    This reverts part of the previous change, so please test again on Mac.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
@@ -11,8 +15,12 @@
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-      perSystem = {pkgs, ...}: {
-        devShells.default = import ./shell.nix {inherit pkgs;};
+      perSystem = {
+        pkgs,
+        inputs',
+        ...
+      }: {
+        devShells.default = import ./shell.nix {inherit pkgs inputs';};
       };
     };
 }
