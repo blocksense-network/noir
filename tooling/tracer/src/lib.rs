49a40e3e146c92f9a73bde99a24c8fc600fdfc02
Stan Manilov <stanislav.manilov@gmail.com>
2024-06-27 16:02:48 +0300

    feat(trace): bugfix: track hidden steps
    This has been an involved one. It turned out that `nargo debug` can skip
    over some steps in the source code, if function calls are inlined, and
    result in a confusing debugging experience. This is why the
    `2_function_calls` test was added: it illustrates the missing steps in
    the trace.
    
    The solution is to track all locations associated with an opcode, not
    just the last one. (See the two calls to `.last()` replaced with `for`
    loops.) The debugger is at least smart enough to link the call sites to
    the opcode, even if they are optimized away by inlining. The updated
    expectation for `2_function_calls` shows that the steps for lines 6, 10,
    and 11 are now included.
    
    To see that the tests pass:
    
    ```
    cargo test trace
    ```
    
    Further changes:
    
     * this commit simplifies the testing scaffold (in
       `tooling/nargo_cli/build.rs`) because instead of comparing a message
       about the number of steps, we now compare the full traces
     * a `SourceLocation` struct is introduced to handle a file-line pair in
       a more hygienic way
     * a `DebugStepResult` is introduced, to simplify the interface between
       `trace_circuit` and `step_debugger`

--- tooling/tracer/src/lib.rs
+++ tooling/tracer/src/lib.rs
@@ -9,16 +9,41 @@ use noir_debugger::foreign_calls::DefaultDebugForeignCallExecutor;
 use noirc_artifacts::debug::DebugArtifact;
 
 use fm::PathString;
+use std::cmp::min;
 use std::path::PathBuf;
 
 use runtime_tracing::{Line, Tracer};
 
 use nargo::NargoError;
 
+#[derive(PartialEq, Debug)]
+struct SourceLocation {
+    filepath: PathString,
+    line_number: isize,
+}
+
+impl SourceLocation {
+    /// Creates a source location that represents an unknown place in the source code.
+    fn create_unknown() -> SourceLocation {
+        SourceLocation { filepath: PathString::from_path(PathBuf::from("?")), line_number: -1 }
+    }
+}
+
+enum DebugStepResult<Error> {
+    /// The debugger reached a new location and the execution is paused at it.
+    Paused(Vec<SourceLocation>),
+    /// The debuger reached the end of the program and finished execution.
+    Finished,
+    /// The debugger reached an error and cannot continue.
+    Error(Error),
+}
+
 pub struct TracingContext<'a, B: BlackBoxFunctionSolver<FieldElement>> {
+    /// Nested structure, used for extracting tracing information by leveraging the functionality
+    /// of `nargo debug`.
     debug_context: DebugContext<'a, B>,
-    current_filepath: PathString, // The path to the file currently pointed at by debugger.
-    current_line: isize,          // The line in the source code that the debugger has reached.
+    /// The call stack at the current moment of tracing.
+    source_locations: Vec<SourceLocation>,
 }
 
 impl<'a, B: BlackBoxFunctionSolver<FieldElement>> TracingContext<'a, B> {
@@ -40,95 +65,103 @@ impl<'a, B: BlackBoxFunctionSolver<FieldElement>> TracingContext<'a, B> {
             unconstrained_functions,
         );
 
-        Self {
-            debug_context,
-            current_filepath: PathString::from_path(PathBuf::new()),
-            current_line: -1isize,
-        }
+        Self { debug_context, source_locations: vec![] }
     }
 
-    /// Extracts the current filepath and line in that file from the debugger, given that the
-    /// relevant debugging information is present.
+    /// Extracts the stack of current source locations from the debugger, given that the
+    /// relevant debugging information is present. In the context of this method, a source location
+    /// is a path to a source file and a line in that file. The most recently called function is
+    /// last in the returned vector/stack.
     ///
     /// Otherwise, returns a &str containing a warning message about the missing data.
-    fn get_current_line_and_filepath(&self) -> Result<(isize, PathString), String> {
+    fn get_current_source_locations(&self) -> Vec<SourceLocation> {
         let call_stack = self.debug_context.get_call_stack();
-        let opcode_location = match call_stack.last() {
-            Some(location) => location,
-            None => {
-                return Err(String::from("Warning: no call stack"));
-            }
-        };
 
-        let locations = self.debug_context.get_source_location_for_opcode_location(opcode_location);
-        let source_location = match locations.last() {
-            Some(location) => location,
-            None => {
-                return Err(String::from("Warning: no source location mapped to opcode"));
-            }
-        };
+        let mut result: Vec<SourceLocation> = vec![];
+        for opcode_location in call_stack {
+            let locations =
+                self.debug_context.get_source_location_for_opcode_location(&opcode_location);
+            for source_location in locations {
+                let filepath = match self.debug_context.get_filepath_for_location(source_location) {
+                    Ok(filepath) => filepath,
+                    Err(error) => {
+                        println!("Warning: could not get filepath for source location: {error}");
+                        result.push(SourceLocation::create_unknown());
+                        continue;
+                    }
+                };
 
-        let filepath = match self.debug_context.get_filepath_for_location(*source_location) {
-            Ok(filepath) => filepath,
-            Err(error) => {
-                return Err(format!(
-                    "Warning: could not get filepath for source location: {error}"
-                ));
-            }
-        };
+                let line_number = match self.debug_context.get_line_for_location(source_location) {
+                    Ok(line) => line as isize + 1,
+                    Err(error) => {
+                        println!("Warning: could not get line for source location: {error}");
+                        result.push(SourceLocation::create_unknown());
+                        continue;
+                    }
+                };
 
-        let current_line = match self.debug_context.get_line_for_location(*source_location) {
-            Ok(line) => line as isize + 1,
-            Err(error) => {
-                return Err(format!("Warning: could not get line for source location: {error}"));
+                result.push(SourceLocation { filepath, line_number })
             }
-        };
+        }
 
-        Ok((current_line, filepath))
+        result
     }
 
     /// Steps the debugger until a new line is reached, or the debugger returns anything other than
-    /// Ok.
+    /// Ok. If there are no source locations associated with the opcodes at any time, the stepping
+    /// is also continued.
     ///
-    /// Propagates the debugger result.
-    fn step_debugger(&mut self) -> DebugCommandResult {
+    /// Returns a dedicated type, which on success contains the call stack at which debugging has
+    /// paused.
+    fn step_debugger(&mut self) -> DebugStepResult<NargoError<FieldElement>> {
         loop {
-            let result = self.debug_context.next_into();
-
-            match &result {
-                DebugCommandResult::Done
-                | DebugCommandResult::Error(_)
-                | DebugCommandResult::BreakpointReached(_) => return result,
+            match self.debug_context.next_into() {
+                DebugCommandResult::Done => return DebugStepResult::Finished,
+                DebugCommandResult::Error(error) => return DebugStepResult::Error(error),
+                DebugCommandResult::BreakpointReached(loc) => {
+                    panic!("Error: Breakpoint unexpected in tracer; loc={loc}")
+                }
                 DebugCommandResult::Ok => (),
             }
 
-            let (current_line, filepath) = match self.get_current_line_and_filepath() {
-                Ok(pair) => pair,
-                Err(warning) => {
-                    println!("{warning}");
-                    continue;
-                }
+            let source_locations = self.get_current_source_locations();
+            if source_locations.is_empty() {
+                println!("Warning: no call stack");
+                continue;
             };
 
-            if self.current_filepath == filepath && self.current_line == current_line {
+            if self.source_locations.len() == source_locations.len()
+                && self.source_locations.last().unwrap() == source_locations.last().unwrap()
+            {
                 // Continue stepping until a new line in the same file is reached, or the current file
                 // has changed.
                 // TODO(coda-bug/r916): a function call could result in an extra step
                 continue;
             }
 
-            self.current_filepath = filepath;
-            self.current_line = current_line;
-            return result;
+            return DebugStepResult::Paused(source_locations);
         }
     }
 
     /// Propagates information about the current execution state to `tracer`.
-    fn update_record(&mut self, tracer: &mut Tracer) {
-        tracer.register_step(
-            &PathBuf::from(self.current_filepath.to_string()),
-            Line(self.current_line as i64),
-        );
+    fn update_record(&mut self, tracer: &mut Tracer, source_locations: Vec<SourceLocation>) {
+        // Find the last index of the previous and current stack traces, until which they are
+        // identical.
+        let mut last_match: isize = -1;
+        for i in 0..min(self.source_locations.len(), source_locations.len()) {
+            if self.source_locations[i] == source_locations[i] {
+                last_match = i as isize;
+                continue;
+            }
+            break;
+        }
+        // For the rest of the indexes of the new call stack: register a step that was performed to
+        // reach that frame of the call stack.
+        for i in ((last_match + 1) as usize)..source_locations.len() {
+            let SourceLocation { filepath, line_number } = &source_locations[i];
+            tracer.register_step(&PathBuf::from(filepath.to_string()), Line(*line_number as i64));
+        }
+        self.source_locations = source_locations;
     }
 }
 
@@ -153,24 +186,20 @@ pub fn trace_circuit<B: BlackBoxFunctionSolver<FieldElement>>(
         return Ok(());
     }
 
-    let mut steps = 0;
-    tracer.start(&PathBuf::from(""), Line(-1));
+    let SourceLocation { filepath, line_number } = SourceLocation::create_unknown();
+    tracer.start(&PathBuf::from(filepath.to_string()), Line(line_number as i64));
     loop {
-        match tracing_context.step_debugger() {
-            DebugCommandResult::Done => break,
-            DebugCommandResult::Ok => steps += 1,
-            DebugCommandResult::Error(err) => {
+        let source_locations = match tracing_context.step_debugger() {
+            DebugStepResult::Finished => break,
+            DebugStepResult::Error(err) => {
                 println!("Error: {err}");
                 break;
             }
-            DebugCommandResult::BreakpointReached(loc) => {
-                panic!("Error: Breakpoint unexpected in tracer; loc={loc}");
-            }
-        }
+            DebugStepResult::Paused(source_locations) => source_locations,
+        };
 
-        tracing_context.update_record(tracer);
+        tracing_context.update_record(tracer, source_locations);
     }
-    println!("Total tracing steps: {steps}");
 
     Ok(())
 }
