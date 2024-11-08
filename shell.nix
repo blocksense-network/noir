c560ced4b1dbff6bbf1151ee0d8b5188488b23cf
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2024-11-08 17:59:46 +0200

    build(nix-shell): Nixify Venir
    Nix develop now packages the Venir project and adds it to $PATH.
    
    Co-authored-by: Petar Atanasov <petar.atanasov@metacraft-labs.com>

--- shell.nix
+++ shell.nix
@@ -2,16 +2,20 @@
 {
   pkgs,
   self',
+  venir-toolchain,
   ...
 }: let
   inherit (pkgs) lib stdenv mkShell;
   inherit (pkgs.darwin.apple_sdk) frameworks;
+  venir = import ./derivation.nix {inherit pkgs self' venir-toolchain;};
 in
   mkShell {
     packages =
       [
         pkgs.alejandra
+        venir
         self'.legacyPackages.rustToolchain
+        # pkgs.rustfilt
       ]
       ++ lib.optionals stdenv.isDarwin [
         pkgs.libiconv
