341882294bb228dbfa27c2696b37a13cf4f980ae
Kamen Mladenov <kamen@syndamia.com>
2024-11-20 13:27:20 +0200

    fix(ssa_gen): Start fv attribute indecies with one ahead
    This eliminates certain crashes which occur with the code in
    insert_fv_instruction_and_results inside the dfg.

--- compiler/noirc_evaluator/src/ssa/ssa_gen/mod.rs
+++ compiler/noirc_evaluator/src/ssa/ssa_gen/mod.rs
@@ -141,8 +141,7 @@ impl<'a> FunctionContext<'a> {
 
         self.builder.terminate_with_return(results);
 
-        self.builder.current_function.dfg.fv_start_id =
-            self.builder.current_function.dfg.num_instructions();
+        self.builder.current_function.dfg.fv_start_id = self.builder.current_function.dfg.num_instructions() + 1;
         for fvexpr in formal_verification_expressions {
             match fvexpr {
                 FvExpression::Ensures(expr) => {
