06a46b68383d9215ca466c1702fe336fdc0a4840
Kamen Mladenov <kamen@syndamia.com>
2024-11-21 10:21:33 +0200

    chore(derivation.nix): Update Venir

--- derivation.nix
+++ derivation.nix
@@ -25,8 +25,8 @@ in
     src = fetchFromGitHub {
       owner = "blocksense-network";
       repo = "Venir";
-      hash = "sha256-SKhqc4fNT128KnLmoyL0xo8vrCGtAZanG3XcMDW8SjM";
-      rev = "1fc4019f06bc8bdf4bba084cc4a404bb686a9c07";
+      hash = "sha256-JseMehoV806Z9aFFY4Zfl44FowS1lmtrQjwZAfrfxNQ";
+      rev = "85c4cdaf64e08893d9f190dfd68b6f9117afc095";
     };
 
     cargoLock = {
