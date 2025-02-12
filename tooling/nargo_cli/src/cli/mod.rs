e0b27d3fa93559be1ad9cb46c2ac818570016fad
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-02-12 12:55:53 +0200

    feat(fmt): Add `nargo format-trace` to CLI
    The `nargo` command now accepts the option "format-trace" for
    prettifying generated output from `nargo trace`. More details and how to
    use it can be found with `nargo format-trace --help`. Do note, the
    original program for this purpose (`format_trace`) hasn't been removed
    yet.

--- tooling/nargo_cli/src/cli/mod.rs
+++ tooling/nargo_cli/src/cli/mod.rs
@@ -32,6 +32,7 @@ mod prove_cmd;
 mod test_cmd;
 mod trace_cmd;
 mod verify_cmd;
+mod fmt_trace_cmd;
 
 const GIT_HASH: &str = env!("GIT_COMMIT");
 const IS_DIRTY: &str = env!("GIT_DIRTY");
@@ -106,6 +107,7 @@ enum NargoCommand {
     Verify(verify_cmd::VerifyCommand),
     Test(test_cmd::TestCommand),
     Trace(trace_cmd::TraceCommand),
+    FormatTrace(fmt_trace_cmd::FmtTraceCommand),
     Info(info_cmd::InfoCommand),
     Lsp(lsp_cmd::LspCommand),
     #[command(hide = true)]
@@ -151,6 +153,7 @@ pub(crate) fn start_cli() -> eyre::Result<()> {
         NargoCommand::Prove(args) => prove_cmd::run(args, config),
         NargoCommand::Verify(args) => verify_cmd::run(args, config),
         NargoCommand::Trace(args) => trace_cmd::run(args, config),
+        NargoCommand::FormatTrace(args) => fmt_trace_cmd::run(args),
         NargoCommand::Check(args) => with_workspace(args, config, check_cmd::run),
         NargoCommand::Compile(args) => with_workspace(args, config, compile_cmd::run),
         NargoCommand::Debug(args) => with_workspace(args, config, debug_cmd::run),
