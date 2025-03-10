379f2bee5ec218ca37aa7babb32055d3064650c8
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-03-10 16:52:27 +0200

    fix(fv): Wrap with an `if`, instructions of unit type
    We now `if` wrap instructions which do not return values but are located
    inside of an `enable_side_effects` block. This includes `constrain` and
    function calls which do not return values.
    
    Moved a failing test to success directory. Also added one more test.

--- compiler/noirc_evaluator/src/ssa/verus_vir_gen/expr_to_vir/patterns.rs
+++ compiler/noirc_evaluator/src/ssa/verus_vir_gen/expr_to_vir/patterns.rs
@@ -7,17 +7,18 @@ use vir::ast_util::ident_binder;
 
 use crate::ssa::verus_vir_gen::*;
 
+use super::exprs::wrap_with_an_if_logic;
+
 fn lhs_value_to_pattern(
     value_id: &ValueId,
     dfg: &DataFlowGraph,
     instruction_id: &InstructionId,
 ) -> Pattern {
     let value_patternx = PatternX::Var { name: id_into_var_ident(*value_id), mutable: false }; // Mutability not supported for the prototype
-    // Dereference type if we are initializing with allocate.
+                                                                                               // Dereference type if we are initializing with allocate.
     let vir_type = if let Instruction::Allocate = dfg[*instruction_id] {
         match dfg[*value_id].get_type().clone() {
-            Type::Reference(inner_type) => {
-                from_noir_type(inner_type.as_ref().clone(), None)},
+            Type::Reference(inner_type) => from_noir_type(inner_type.as_ref().clone(), None),
             _ => unreachable!(),
         }
     } else {
@@ -97,20 +98,38 @@ pub(crate) fn instruction_to_stmt(
     );
 
     match dfg.instruction_results(instruction_id).len() {
-        0 => Spanned::new(
-            build_span(
-                &instruction_id,
-                format!("Instruction({})", instruction_id),
-                Some(dfg.get_call_stack(instruction_id)),
-            ),
-            StmtX::Expr(instruction_to_expr(
-                instruction_id,
-                instruction,
-                mode,
-                dfg,
-                current_context,
-            )),
-        ),
+        0 => {
+            let instruction_as_expr =
+                instruction_to_expr(instruction_id, instruction, mode, dfg, current_context);
+            
+            if let Some(condition_id) = current_context.side_effects_condition {
+                // We have to wrap with an `if` our expression.
+                let if_wrapped_expr = wrap_with_an_if_logic(
+                    condition_id,
+                    instruction_as_expr,
+                    None,
+                    dfg,
+                    current_context.result_id_fixer,
+                );
+                Spanned::new(
+                    build_span(
+                        &instruction_id,
+                        format!("Instruction({}) wrapped with if", instruction_id),
+                        Some(dfg.get_call_stack(instruction_id)),
+                    ),
+                    StmtX::Expr(if_wrapped_expr),
+                )
+            } else {
+                Spanned::new(
+                    build_span(
+                        &instruction_id,
+                        format!("Instruction({})", instruction_id),
+                        Some(dfg.get_call_stack(instruction_id)),
+                    ),
+                    StmtX::Expr(instruction_as_expr),
+                )
+            }
+        }
         _ => Spanned::new(
             instruction_span,
             StmtX::Decl {
