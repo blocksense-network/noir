a09f88d3019aac97f1aaae860c1a0bcd676f459f
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-04-02 13:49:21 +0300

    feat(fv): Skeleton logic for `nargo fv`
    Set up functions and expand Noir structures to prepare for later when we
    will be implementing and connecting all formal verification modules.

--- tooling/nargo_cli/src/cli/fv_cmd.rs
+++ tooling/nargo_cli/src/cli/fv_cmd.rs
@@ -1,9 +1,11 @@
 use clap::Args;
-use nargo::workspace::Workspace;
+use fm::FileManager;
+use nargo::{ops::report_errors, prepare_package, workspace::Workspace};
 use nargo_toml::PackageSelection;
-use noirc_driver::CompileOptions;
+use noirc_driver::{link_to_debug_crate, CompilationResult, CompileOptions, CompiledProgram};
+use noirc_frontend::debug::DebugInstrumenter;
 
-use crate::errors::CliError;
+use crate::{cli::compile_cmd::parse_workspace, errors::CliError};
 
 use super::{LockType, PackageOptions, WorkspaceCommand};
 
@@ -34,5 +36,38 @@ impl WorkspaceCommand for FormalVerifyCommand {
 }
 
 pub(crate) fn run(args: FormalVerifyCommand, workspace: Workspace) -> Result<(), CliError> {
+    let (workspace_file_manager, parsed_files) = parse_workspace(&workspace, false);
+    let binary_packages = workspace.into_iter().filter(|package| package.is_binary());
+
+    for package in binary_packages {
+        let (mut context, crate_id) =
+            prepare_package(&workspace_file_manager, &parsed_files, package);
+        link_to_debug_crate(&mut context, crate_id);
+        context.debug_instrumenter = DebugInstrumenter::default();
+        context.package_build_path = workspace.package_build_path(package);
+        context.perform_formal_verification = true;
+
+        let compiled_program =
+            noirc_driver::compile_main(&mut context, crate_id, &args.compile_options, None);
+
+        // We want to formally verify only compilable programs
+        report_errors(
+            compiled_program.clone(),
+            &workspace_file_manager,
+            args.compile_options.deny_warnings,
+            true, // We don't want to report compile related warnings
+        )?;
+
+        z3_verify(compiled_program, &workspace_file_manager, args.compile_options.deny_warnings)?
+    }
+
+    Ok(())
+}
+
+fn z3_verify(
+    compiled_program: CompilationResult<CompiledProgram>,
+    workspace_file_manager: &FileManager,
+    deny_warnings: bool,
+) -> Result<(), CliError> {
     todo!()
 }
