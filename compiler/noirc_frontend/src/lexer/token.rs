731d9511149588c94e3248663db5406ee834bdf9
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-04-04 17:55:59 +0300

    feat(fv): Lex quantifiers

--- compiler/noirc_frontend/src/lexer/token.rs
+++ compiler/noirc_frontend/src/lexer/token.rs
@@ -1166,8 +1166,10 @@ pub enum Keyword {
     Dep,
     Else,
     Enum,
+    Exists,
     Fn,
     For,
+    Forall,
     Global,
     If,
     Impl,
@@ -1232,6 +1234,8 @@ impl fmt::Display for Keyword {
             Keyword::Use => write!(f, "use"),
             Keyword::Where => write!(f, "where"),
             Keyword::While => write!(f, "while"),
+            Keyword::Exists => write!(f, "exists"),
+            Keyword::Forall => write!(f, "forall"),
         }
     }
 }
@@ -1253,8 +1257,10 @@ impl Keyword {
             "dep" => Keyword::Dep,
             "else" => Keyword::Else,
             "enum" => Keyword::Enum,
+            "exists" => Keyword::Exists,
             "fn" => Keyword::Fn,
             "for" => Keyword::For,
+            "forall" => Keyword::Forall,
             "global" => Keyword::Global,
             "if" => Keyword::If,
             "impl" => Keyword::Impl,
