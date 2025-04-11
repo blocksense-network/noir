0a97e028c8d4e34ed67807063e2216f20a0e4fed
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-04-11 13:22:20 +0300

    feat(fv): Semantic analysis for quantifiers
    Added type checking and name resolution for quantifier expressions.
    This happens during the elaboration phase in the Noir compiler.
    
    Because of a limitation of the Noir type system we initialize the
    quantifier indexes with type `Unbounded(Any)`. Those types automatically
    bound themseleves to the first type they interact with. In result the
    user sometimes has to do some extra type casting when using indexes.

--- compiler/noirc_frontend/src/hir/comptime/errors.rs
+++ compiler/noirc_frontend/src/hir/comptime/errors.rs
@@ -145,6 +145,9 @@ pub enum InterpreterError {
     UnquoteFoundDuringEvaluation {
         location: Location,
     },
+    QuantifierFoundDuringEvaluation {
+        location: Location,
+    },
     DebugEvaluateComptime {
         diagnostic: CustomDiagnostic,
         location: Location,
@@ -301,6 +304,7 @@ impl InterpreterError {
             | InterpreterError::NonEnumInConstructor { location, .. }
             | InterpreterError::CannotInlineMacro { location, .. }
             | InterpreterError::UnquoteFoundDuringEvaluation { location, .. }
+            | InterpreterError::QuantifierFoundDuringEvaluation { location, .. }
             | InterpreterError::UnsupportedTopLevelItemUnquote { location, .. }
             | InterpreterError::ComptimeDependencyCycle { location, .. }
             | InterpreterError::Unimplemented { location, .. }
@@ -523,6 +527,11 @@ impl<'a> From<&'a InterpreterError> for CustomDiagnostic {
             InterpreterError::UnquoteFoundDuringEvaluation { location } => {
                 let msg = "Unquote found during comptime evaluation".into();
                 let secondary = "This is a bug".into();
+                CustomDiagnostic::simple_error(msg, secondary, *location)
+            }
+            InterpreterError::QuantifierFoundDuringEvaluation { location } => {
+                let msg = "Quantifier found during comptime evaluation".into();
+                let secondary = "Quantifiers are allowed only in specification code".into();
                 CustomDiagnostic::simple_error(msg, secondary, *location)
             }
             InterpreterError::DebugEvaluateComptime { diagnostic, .. } => diagnostic.clone(),
