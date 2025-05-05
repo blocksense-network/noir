ebdcdd9c641bbaff5fa5a8ddd0284d2e6a78073b
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-05-05 11:54:12 +0300

    test(fv): Expand testing logic
    Now there are two directories for testing formal verification
    functionality. Them being `formal_verify_failure` and
    `formal_verify_success`.
    
    Added some tests which check if the semantic analysis of fv annotation
    expressions is correct.

--- tooling/nargo_cli/build.rs
+++ tooling/nargo_cli/build.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
@@ -64,6 +65,8 @@ fn main() {
         &test_dir,
         &IGNORED_NARGO_EXPAND_COMPILE_SUCCESS_WITH_BUG_TESTS,
     );
+    generate_formal_verify_success_tests(&mut test_file, &test_dir);
+    generate_formal_verify_failure_tests(&mut test_file, &test_dir);
 }
 
 /// Some tests are explicitly ignored in brillig due to them failing.
@@ -765,4 +768,81 @@ mod nargo_expand_{test_type} {{
     }
 
     writeln!(test_file, "}}").unwrap();
+}
+
+/// Tests which must succeed after undergoing formal verification.
+fn generate_formal_verify_success_tests(test_file: &mut File, test_data_dir: &Path) {
+    let test_type = "formal_verify_success";
+    let test_cases = read_test_cases(test_data_dir, test_type);
+    for (test_name, test_dir) in test_cases {
+        write!(
+            test_file,
+            r#"
+#[test]
+fn formal_verify_success_{test_name}() {{
+    let test_program_dir = PathBuf::from("{test_dir}");
+
+    let mut cmd = Command::cargo_bin("nargo").unwrap();
+    cmd.arg("--program-dir").arg(test_program_dir);
+    cmd.arg("formal-verify");
+
+    cmd.assert().success();
+}}
+            "#,
+            test_dir = test_dir.display(),
+        )
+        .expect("Could not write templated test file.");
+    }
+}
+
+/// Tests which must fail during formal verification.
+fn generate_formal_verify_failure_tests(test_file: &mut File, test_data_dir: &Path) {
+    let test_type = "formal_verify_failure";
+    let test_cases = read_test_cases(test_data_dir, test_type);
+
+    let expected_messages = HashMap::from([("simple_add", vec!["Cannot satisfy constraint"])]);
+
+    for (test_name, test_dir) in test_cases {
+        write!(
+            test_file,
+            r#"
+#[test]
+fn formal_verify_failure_{test_name}() {{
+    let test_program_dir = PathBuf::from("{test_dir}");
+
+    let mut cmd = Command::cargo_bin("nargo").unwrap();
+    cmd.arg("--program-dir").arg(test_program_dir);
+    cmd.arg("formal-verify");
+
+    cmd.assert().failure().stderr(
+        predicate::str::contains("The application panicked (crashed).")
+        .or(predicate::str::contains("error:")),
+    );"#,
+            test_dir = test_dir.display(),
+        )
+        .expect("Could not write templated test file.");
+
+        // Not all tests have expected messages, so match.
+        match expected_messages.get(test_name.as_str()) {
+            Some(messages) => {
+                for message in messages.iter() {
+                    write!(
+                        test_file,
+                        r#"
+    cmd.assert().failure().stderr(predicate::str::contains("{message}"));"#
+                    )
+                    .expect("Could not write templated test file.");
+                }
+            }
+            None => {}
+        }
+
+        write!(
+            test_file,
+            r#"
+}}
+"#
+        )
+        .expect("Could not write templated test file.");
+    }
 }
