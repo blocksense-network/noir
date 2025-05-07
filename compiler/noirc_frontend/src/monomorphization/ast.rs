02848920d0eb732b541517274e5948eedbab590b
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-05-07 17:33:48 +0300

    feat(fv): Monomorphize FV annotations

--- compiler/noirc_frontend/src/monomorphization/ast.rs
+++ compiler/noirc_frontend/src/monomorphization/ast.rs
@@ -314,6 +314,13 @@ impl std::fmt::Display for InlineType {
 }
 
 #[derive(Debug, Clone, Hash)]
+pub enum MonomorphizedFvAttribute {
+    Ensures(Expression),
+    Requires(Expression),
+    Ghost,
+}
+
+#[derive(Debug, Clone, Hash)]
 pub struct Function {
     pub id: FuncId,
     pub name: String,
@@ -325,6 +332,7 @@ pub struct Function {
     pub unconstrained: bool,
     pub inline_type: InlineType,
     pub func_sig: FunctionSignature,
+    pub formal_verification_attributes: Vec<MonomorphizedFvAttribute>,
 }
 
 /// Compared to hir_def::types::Type, this monomorphized Type has:
