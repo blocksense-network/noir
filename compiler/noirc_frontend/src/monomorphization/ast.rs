4fa9683ab6c7ecc6ebdf534b576a61873f56112c
Aristotelis Papanis <aristotelis.papanis@blocksense.network>
2025-05-09 14:00:20 +0300

    feat(fv): Monomorphize quantifier expressions

--- compiler/noirc_frontend/src/monomorphization/ast.rs
+++ compiler/noirc_frontend/src/monomorphization/ast.rs
@@ -7,7 +7,7 @@ use noirc_errors::{
 };
 
 use crate::{
-    ast::{BinaryOpKind, IntegerBitSize},
+    ast::{BinaryOpKind, IntegerBitSize, QuantifierType},
     hir_def::expr::Constructor,
     shared::Signedness,
     signed_field::SignedField,
@@ -54,6 +54,7 @@ pub enum Expression {
     Drop(Box<Expression>),
     Break,
     Continue,
+    Quant(QuantifierType, Vec<Ident> ,Box<Expression>),
 }
 
 impl Expression {
